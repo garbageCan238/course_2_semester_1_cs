bc95fa6ccc2375a6bf4a0b02f21413e6b46cc634
Rashid <pchela25000@gmail.com>
2024-09-04 00:47:20 +0300

    first5

--- course_2_semester_1_cs/Program.cs
+++ course_2_semester_1_cs/Program.cs
@@ -1,10 +1,17 @@
-﻿internal class Program
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+using static System.Collections.Specialized.BitVector32;
+
+internal class Program
 {
+    [Serializable]
     class UninterruptivlePowerSupply
     {
-        public string? manufacturer;
-        public string? brand;
-        public int? capacity;
+        public string? manufacturer { get; set; }
+        public string? brand { get; set; }
+        public int? capacity { get; set; }
 
         protected string[] possibleManufacturers = ["Apple", "Samsung", "Hp", "Dell", "Xiaomi", "Huawei", "Techno", "Google", "OnePlus", "Alcatel", "Asus", "Lenovo", "Vivo"];
         protected string[] possibleBrands = ["S1", "S2", "S3", "Redmi", "Nova", "Spark", "A60", "Go", "K15", "Galaxy", "15", "13", "B256"];
@@ -15,8 +22,14 @@
             this.brand = possibleBrands[new Random().Next(0, possibleBrands.Length)];
             this.capacity = new Random().Next(0, 1000);
         }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 
+    [Serializable]
     class PowerSupplies
     {
         private int _count;
@@ -31,7 +44,7 @@
                 _count = value;
             }
         }
-        private UninterruptivlePowerSupply[] supplies;
+        public UninterruptivlePowerSupply[] supplies { get; set; }
 
         public PowerSupplies(int count)
         {
@@ -54,11 +67,35 @@
 
     private static void Main(string[] args)
     {
+        var singlePowerSupply = new UninterruptivlePowerSupply();
+
         var powerSupplies = new PowerSupplies(10);
         for (int i = 0; i < powerSupplies.Count; i++)
         {
             powerSupplies[i] = new UninterruptivlePowerSupply();
-            Console.WriteLine($"manufacturer: {powerSupplies[i].manufacturer}, brand: {powerSupplies[i].brand}, capacity: {powerSupplies[i].capacity}");
         }
+
+        using (var fs = new FileStream("single_power_supply.txt", FileMode.Create, FileAccess.Write))
+        {
+            JsonSerializer.Serialize(fs, singlePowerSupply);
+        }
+
+        using (var fs = new FileStream("power_supplies.txt", FileMode.Create, FileAccess.Write))
+        {
+            JsonSerializer.Serialize(fs, powerSupplies);
+        }
+
+        UninterruptivlePowerSupply? deserializedPowerSupply = null;
+        PowerSupplies? deserializedPowerSupplies = null;
+
+        using (var jsonFileReader = File.OpenText("single_power_supply.txt"))
+        {
+            deserializedPowerSupply = JsonSerializer.Deserialize<UninterruptivlePowerSupply> (jsonFileReader.ReadToEnd(),
+            new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+        }
+
     }
 }
