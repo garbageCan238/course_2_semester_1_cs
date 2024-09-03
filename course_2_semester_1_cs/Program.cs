73b2dd9da6f3ecdcf5faef7a3711844475c8973a
Rashid <pchela25000@gmail.com>
2024-09-03 23:05:27 +0300

    change capacity to int

--- course_2_semester_1_cs/Program.cs
+++ course_2_semester_1_cs/Program.cs
@@ -4,7 +4,7 @@
     {
         public string? manufacturer;
         public string? brand;
-        public decimal? capacity;
+        public int? capacity;
     }
 
     private static void Main(string[] args)
@@ -13,7 +13,7 @@
 
         powerSupply.manufacturer = "Samsung";
         powerSupply.brand = "Apple";
-        powerSupply.capacity = (decimal)235.124;
+        powerSupply.capacity = 500;
 
         Console.WriteLine($"manufacturer: {powerSupply.manufacturer}, brand: {powerSupply.brand}, capacity: {powerSupply.capacity}");
     }
