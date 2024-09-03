db15ea3b1d06d82a092027e9ea80074423689784
Rashid <pchela25000@gmail.com>
2024-09-03 22:46:32 +0300

    done

--- course_2_semester_1_cs/Program.cs
+++ course_2_semester_1_cs/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using course_2_semester_1_cs;
+
+var powerSupply = new UninterruptivlePowerSupply();
+
+powerSupply.manufacturer = "Samsung";
+powerSupply.brand = "Apple";
+powerSupply.capacity = (decimal)235.124;
+
+Console.WriteLine($"manufacturer: {powerSupply.manufacturer}, brand: {powerSupply.brand}, capacity: {powerSupply.capacity}");
---
+++ course_2_semester_1_cs/UninterruptivlePowerSupply.cs
@@ -0,0 +1,9 @@
+﻿namespace course_2_semester_1_cs
+{
+    class UninterruptivlePowerSupply
+    {
+        public string? manufacturer;
+        public string? brand;
+        public decimal? capacity;
+    }
+}
