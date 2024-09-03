546a2ac55c968a2b8235d2e2896b8bc5785c828a
Rashid <pchela25000@gmail.com>
2024-09-03 22:54:58 +0300

    done again

--- course_2_semester_1_cs/Program.cs
+++ course_2_semester_1_cs/Program.cs
@@ -1,9 +1,20 @@
-﻿using course_2_semester_1_cs;
+﻿internal class Program
+{
+    class UninterruptivlePowerSupply
+    {
+        public string? manufacturer;
+        public string? brand;
+        public decimal? capacity;
+    }
 
-var powerSupply = new UninterruptivlePowerSupply();
+    private static void Main(string[] args)
+    {
+        var powerSupply = new UninterruptivlePowerSupply();
 
-powerSupply.manufacturer = "Samsung";
-powerSupply.brand = "Apple";
-powerSupply.capacity = (decimal)235.124;
+        powerSupply.manufacturer = "Samsung";
+        powerSupply.brand = "Apple";
+        powerSupply.capacity = (decimal)235.124;
 
-Console.WriteLine($"manufacturer: {powerSupply.manufacturer}, brand: {powerSupply.brand}, capacity: {powerSupply.capacity}");
+        Console.WriteLine($"manufacturer: {powerSupply.manufacturer}, brand: {powerSupply.brand}, capacity: {powerSupply.capacity}");
+    }
+}
